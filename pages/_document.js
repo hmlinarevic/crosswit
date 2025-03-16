4742a200460b474f3f34a41425959fcff41323fd
hmlinarevic <hrvoje.mlinarevic@gmail.com>
2025-03-16 20:05:01 +0100

    feat: add pages and start with entry point code

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Righteous&family=Caveat&family=Roboto&family=Merriweather&family=Ubuntu:wght@400;700&family=Ubuntu+Mono&family=Source+Code+Pro&family=Titillium+Web&display=swap"
+                        rel="stylesheet"
+                    />
+                </Head>
+
+                <body>
+                    <Main />
+                    <div id="overlays" />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import Fade from "./components/Fade";
+import Button from "./components/Button";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showContent, setShowContent] = useState(true);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="dark:bg-base grid h-screen place-content-center">
+        <Fade
+          toggler={showContent}
+          duration={500}
+          onEnd={() => console.log('change page...')}
+          className=""
+        >
+          <div className="flex select-none items-center justify-center font-titilliumWeb text-4xl text-rose">
+            <span className="font-righteous">CR</span>
+            {/* logo */}
+            <span className="border-1 p-8 grid place-content-center">logo here...</span>
+            {/* <Image
+              // className="relative top-[10px]"
+              src={brain2Png}
+              style={{
+                width: "32px",
+                height: "auto",
+                marginLeft: "2.5px",
+                marginRight: "2.5px",
+              }}
+              alt="abstract brain symbol"
+            /> */}
+            <span className="font-righteous">SSWIT</span>
+          </div>
+
+          <span className="block text-center font-caveat text-xl text-love">
+            {"Word Search & Memory Trainer"}
+          </span>
+
+          {/* buttons */}
+          <div className="mx-auto mt-3 flex h-[30px] w-[200px] justify-between">
+            <Button
+              className="mr-2 h-auto w-full text-sm hover:border-rose hover:bg-rose hover:bg-opacity-10 hover:text-rose"
+              onClick={() => console.log('handle play click...')}
+            >
+              play
+            </Button>
+
+            <Button
+              className="w-full text-sm hover:border-rose hover:bg-rose hover:bg-opacity-10 hover:text-rose"
+              onClick={() => console.log('go to about page...')}
+            >
+              about
+            </Button>
+          </div>
+        </Fade>
+      </section>
     </>
   )
 }
