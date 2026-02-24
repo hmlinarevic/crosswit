2a393ed4273f02e34d2c782ae0177d5c6af6b892
hmlinarevic <hrvoje.mlinarevic@gmail.com>
2026-02-24 19:25:44 +0100

    feat: add basePath and nginx config for /projects/crosswit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/projects/crosswit",
   // Next 16: Turbopack is default for dev + build. Use `next build --webpack` to opt out.
 };
 
